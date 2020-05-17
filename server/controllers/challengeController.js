ae0b045c9b4fceb136d14d6425e3a9f7c959d49e
Rashmi Sharma <rksharma2679@gmail.com>
2020-05-17 10:41:27 -0400

    Adding challenge controller

---
+++ server/controllers/challengeController.js
@@ -0,0 +1,47 @@
+const ObjectId = require("mongoose").Types.ObjectId;
+const db = require("../models");
+
+// Defining methods for the runningStatsController
+module.exports = {
+  findAll: function(req, res) {
+    db.Challenge
+        .find()
+        .then(challenges => {
+          res.json({ challenges });
+        })
+        .catch(err => res.status(422).json(err));
+ },
+  findById: function(req, res) {
+    db.Challenge
+        .find({ _id: req.params.id })
+        .then(challenge => {
+          res.json({ challenge });
+        })
+        .catch(err => res.status(422).json(err));
+  },
+  create: function(req, res) {
+    db.Challenge
+      .create(req.body)
+      .then((dbChallenge) => {
+        // If the User was updated successfully, send it back to the client
+        res.json(dbChallenge);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  update: function(req, res) {
+    db.Challenge
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbChallenge => {
+        console.log(dbChallenge);
+        res.json(dbChallenge);
+      })
+      .catch(err => res.status(422).json(err));
+  },
+  remove: function(req, res) {
+    db.Challenge
+        .findOneAndDelete({ _id: req.params.id })
+        .then(dbChallenge => res.json(dbChallenge))
+        .catch(err => res.status(422).json(err));
+  }
+  
+};

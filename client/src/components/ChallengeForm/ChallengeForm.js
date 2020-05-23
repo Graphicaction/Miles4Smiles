90bdf7d5a1843a94b054b1975b5e8179a7d50dd8
Rachel Humble <rachelhumble@me.com>
2020-05-23 10:14:27 -0400

    Challenge modal

---
+++ client/src/components/ChallengeForm/ChallengeForm.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+
+function ChallengeForm() {
+
+    return (
+        <>
+            <form>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Enter a user to challenge</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"></input>
+                    <small id="emailHelp" class="form-text text-muted">***Later this will be users db search***</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Which Biz will you run for?</label>
+                    <input class="form-control" type="text" placeholder="Enter business name"></input>
+                    <small id="emailHelp" class="form-text text-muted">***Later this will be Local Business API search***</small>
+                </div>
+                <div class="form-group">
+                    <label for="exampleFormControlSelect1">Select Biz type:</label>
+                    <select class="form-control" id="exampleFormControlSelect1">
+                        <option>Retail</option>
+                        <option>Education</option>
+                        <option>Food/Beverage</option>
+                        <option>Tech</option>
+                        <option>Other</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label for="exampleInputEmail1">Let's talk milage 🏁</label>
+                    <input class="form-control form-control-sm" type="text" placeholder="Enter proposed challenge distance in miles"></input>
+                    <input class="form-control form-control-sm" type="text" placeholder="Enter donation amount per mile in USD"></input>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default ChallengeForm

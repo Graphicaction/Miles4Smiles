ad5d2e435e8e8cdccd92747007823573e85062ae
Steffield <steffilebby@gmail.com>
2020-05-24 11:56:57 -0400

    added countup package in dashboard and jdenticon in usercard

--- client/src/components/UserCard/UserCard.js
+++ client/src/components/UserCard/UserCard.js
@@ -1,14 +1,19 @@
 import React from "react";
 import "./UserCard.css"
+import Jdenticon from "react-jdenticon";
+// import { PromiseProvider } from "mongoose";
 // import RunningStats from "../components/RunningStats/RunningStats";
 
 
-const UserCard =() => {
+const UserCard =(props) => {
+
   return(
     <>
     <div className="card" style={{width: "18rem"}}>
       <div className="card-body ">
+        <Jdenticon className="avatar" size="48" value="test" float="right"></Jdenticon>
         <h5 className="card-title justify-content-center">Username</h5>
+
         <h6 className="card-subtitle mb-2 text-muted"><i className="fa fa-location"></i>Location</h6>
         <hr></hr>
         <p className="card-text pace">Average mile pace: xx</p>

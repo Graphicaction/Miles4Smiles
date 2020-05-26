a9740c899c05b51eb78e03504e4c67329c2a1ad2
Rashmi Sharma <rksharma2679@gmail.com>
2020-05-26 10:27:24 -0400

    Creating challenge context

--- server/config/keys.js
+++ server/config/keys.js
@@ -5,3 +5,9 @@ if (process.env.NODE_ENV==="production"){
 } else {
   module.exports = require("./dev")
 }
+
+// module.exports ={
+//     googleClientID: "668734697594-089art6ir5lju3leprfm0ip8kt06tmvr.apps.googleusercontent.com",
+//     googleClientSecret: "3kaLwafWaHzGeO4G9-T-iDTr",
+//     COOKIE_KEY: 'bliblablup'
+//   };
--- server/passport/GithubStrategy.js
+++ server/passport/GithubStrategy.js
@@ -1,50 +1,50 @@
-const passport = require("passport");
-const GithubStrategy = require("passport-github2").Strategy;
-const mongoose = require("mongoose");
-const User = mongoose.model('User');
-const keys = require("../config/keys");
+// const passport = require("passport");
+// const GithubStrategy = require("passport-github2").Strategy;
+// const mongoose = require("mongoose");
+// const User = mongoose.model('User');
+// const keys = require("../config/keys");
 
-//place mongo generated id in the cookie
-passport.serializeUser((user, done) =>{
-  done(null, user.id);
-});
-//get id out of cookie
-passport.deserializeUser((id, done)=>{
-  User.findById(id)
-  .then(user => {
-    done(null, user);
-  });
-});
+// //place mongo generated id in the cookie
+// passport.serializeUser((user, done) =>{
+//   done(null, user.id);
+// });
+// //get id out of cookie
+// passport.deserializeUser((id, done)=>{
+//   User.findById(id)
+//   .then(user => {
+//     done(null, user);
+//   });
+// });
 
-passport.use(
-  new GithubStrategy(
-    {
-      clientID: keys.GITHUB_CLIENT_ID,
-      clientSecret: keys.GITHUB_CLIENT_SECRET,
-      callbackURL: "https://localhost/3001/auth/github/callback",
-      proxy: true
-    }, 
-    (accessToken, refreshToken, profile, done) => {
-      User.findOne({ githubId : profile.id })
-      .then((existingUser)=>{
-        //if already signed up with that googleid no new user
-        if(existingUser){
-          done(null, existingUser);
-        } else {
-          console.log("accessToken", accessToken);
-          console.log("refresh Token", refreshToken);
-          console.log("profile", profile);
-          // new User({ 
-          //   googleId: profile.id,
-          //   firstName: profile.name.givenName,
-          //   lastName: profile.name.familyName,
-          //   username: profile.displayName 
-          //   // password:""
-          // }).save()
-          // .then(user => done(null, user));
+// passport.use(
+//   new GithubStrategy(
+//     {
+//       clientID: keys.GITHUB_CLIENT_ID,
+//       clientSecret: keys.GITHUB_CLIENT_SECRET,
+//       callbackURL: "https://localhost/3001/auth/github/callback",
+//       proxy: true
+//     }, 
+//     (accessToken, refreshToken, profile, done) => {
+//       User.findOne({ githubId : profile.id })
+//       .then((existingUser)=>{
+//         //if already signed up with that googleid no new user
+//         if(existingUser){
+//           done(null, existingUser);
+//         } else {
+//           console.log("accessToken", accessToken);
+//           console.log("refresh Token", refreshToken);
+//           console.log("profile", profile);
+//           // new User({ 
+//           //   googleId: profile.id,
+//           //   firstName: profile.name.givenName,
+//           //   lastName: profile.name.familyName,
+//           //   username: profile.displayName 
+//           //   // password:""
+//           // }).save()
+//           // .then(user => done(null, user));
 
-          }
-        })
+//           }
+//         })
 
-      })
-  );
+//       })
+//   );

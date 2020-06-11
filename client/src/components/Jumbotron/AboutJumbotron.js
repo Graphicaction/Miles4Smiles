c2b6b55af2f93809f0c1ccde74e3b9601a25d61d
Steffield <steffilebby@gmail.com>
2020-06-10 20:58:27 -0400

    image fix?

--- client/src/components/Jumbotron/AboutJumbotron.js
+++ client/src/components/Jumbotron/AboutJumbotron.js
@@ -6,7 +6,7 @@ const AboutJumbotron = ({ children }) => (
   <div className="jumbotron jumbotron-fluid about-jumbotron">
     <div className="about-container">
       <h1 className="about-title">How it works</h1>
-      <h1 className="about-title">&#9207;</h1>
+      {/* <h1 className="about-title">&#9207;</h1> */}
     </div>
   </div>
 );
